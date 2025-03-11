f186ce36825986df5398b0a8ddf8dc90412cf326
ksu_smolyar <ksu_smolyar@mail.ru>
2025-03-11 11:20:08 +0400

    fix logo

--- src/shared/ui/Logo/ui/Logo.tsx
+++ src/shared/ui/Logo/ui/Logo.tsx
@@ -2,5 +2,5 @@ import styles from '../logo.module.css'
 import Image from 'next/image'
 
 export const Logo = () => {
-  return <Image className={styles.logo} src={'/learn.png'} alt={'лупа'} width={24} height={24} />
+  return <Image className={styles.logo} src={'/learn.png'} alt={'лупа'} width={100} height={100} />
 }
